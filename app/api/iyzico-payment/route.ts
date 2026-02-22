a1a6a04b85222ee9238b0489bb7027cffe77e4a7
SPEED5858 <gokhanaraz3@gmail.com>
2026-02-22 03:15:57 +0300

    remove api temporary

--- app/api/iyzico-payment/route.ts
+++
@@ -1,9 +0,0 @@
-
-import { NextResponse } from "next/server";
-const Iyzipay = require("iyzipay");
-
-export async function POST(req: Request) {
-  return NextResponse.json({
-    message: "iyzico endpoint ready. Configure API keys next."
-  });
-}
